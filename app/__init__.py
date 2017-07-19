93524cb7a58b315b31fcc04ca8a276e7c6327841
Mohammad Arsalan Javed <arsalanjaved2010@gmail.com>
2017-07-19 13:50:12 +0500

    commiting on new repositry

---
+++ app/__init__.py
@@ -0,0 +1,34 @@
+# Import flask and template operators
+from flask import Flask, render_template
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager 
+
+#from app.mod_auth.models import User
+
+app = Flask(__name__)
+
+app.config.from_object('config')
+
+db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+'''@login_manager.user_loader
+def user_loader(user_id):
+    """Given *user_id*, return the associated User object.
+
+    :param unicode user_id: user_id (email) user to retrieve
+    """
+    return User.query.get(user_id)
+'''
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html'), 404
+
+from app.controllers.controllers import * 
+
+db.create_all()
