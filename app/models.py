eadbe0e33cae7e3bd2b5a641a5bcd56c973f9c7d
Mohammad Arsalan Javed <arsalanjaved2010@gmail.com>
2017-07-23 17:25:30 +0500

    updated datetime to date and int to enum in blancesheet table

--- app/models.py
+++ app/models.py
@@ -32,6 +32,7 @@ class User(db.Model):
 
 class Employees(db.Model):
 
+
     id = db.Column(db.Integer, primary_key = True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
     first_name = db.Column(db.String(45), nullable = False)
@@ -59,8 +60,8 @@ class Balance_sheet(db.Model):
 
     id = db.Column(db.Integer, primary_key = True)
     emp_id = db.Column(db.Integer, db.ForeignKey('employees.id'), nullable = False)
-    from_date = db.Column(db.DateTime(), nullable = False)
-    to_date = db.Column(db.DateTime(), nullable = False)
+    from_date = db.Column(db.Date(), nullable = False)
+    to_date = db.Column(db.Date(), nullable = False)
     leave_type = db.Column(db.Enum('General','Medical'), nullable = False)
     purpose = db.Column(db.String(200), nullable = False)
     pay = db.Column(db.Enum('Payed','Unpayed'), nullable = False)
@@ -68,6 +69,8 @@ class Balance_sheet(db.Model):
     manager_remark = db.Column(db.String(128), nullable = True)
     hr_approval = db.Column(db.Integer, nullable = False)
     manager_approval = db.Column(db.Integer, nullable = False)
+    hr_approval = db.Column(db.Enum('Approved','Unapproved'), nullable = True)
+    manager_approval = db.Column(db.Enum('Approved','Unapproved'), nullable = True)
 
     def __init__(self, emp_id, from_date, to_date, leave_type, purpose, pay, hr_remark, manager_remark,
         hr_approval, manager_approval):
