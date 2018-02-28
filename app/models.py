1bde1909b2b202a59b32ac2803b09e6811a8f548
Salik Khan <m.salikkhan95@gmail.com>
2018-02-28 22:00:49 +0500

    role->user, hr_approval->balance_sheet, leaves_remaing->employees(removed)

--- app/models.py
+++ app/models.py
@@ -6,6 +6,7 @@ class User(db.Model):
 	id = db.Column(db.Integer, primary_key = True)
 	email = db.Column(db.String(128), nullable = False, unique = True)
 	password = db.Column(db.String(128), nullable = False)
+	role = db.Column(db.Enum('Employee','HR Manager', 'General Manager'), nullable = False)
 
 	def is_active(self):
 		"""True, as all users are active."""
@@ -23,9 +24,10 @@ class User(db.Model):
 		"""False, as anonymous users aren't supported."""
 		return False
 
-	def __init__(self, email, password):
+	def __init__(self, email, password, role):
 		self.email = email
 		self.password = password
+		self.role = role
 
 	def __repr__(self):
 		return '%d' % (self.id)
@@ -39,27 +41,26 @@ class Employees(db.Model):
 	reporting_manager_id = db.Column(db.Integer, db.ForeignKey('employees.id'), nullable = True)
 	designation = db.Column(db.String(45), nullable = False)
 	department = db.Column(db.String(45), nullable = False)
-	general_leaves_remaining = db.Column(db.Integer, nullable = False)
-	medical_leaves_remaining = db.Column(db.Integer, nullable = False)
 	general_leaves_availed = db.Column(db.Integer, nullable = False)
 	medical_leaves_availed = db.Column(db.Integer, nullable = False)
 	date_of_joining = db.Column(db.Date(), nullable = False)
 
-	user = db.relationship('User', uselist=False, backref=db.backref('employee', uselist=False), lazy='joined', foreign_keys=[user_id])
+	user = db.relationship('User', uselist=False, backref=db.backref('employee', uselist=False), 
+		lazy='joined', foreign_keys=[user_id])
+	
 	balance_sheet = db.relationship('Balance_sheet', backref='employee', lazy='joined')
-	manager = db.relationship('Employees', backref=db.backref('subordinates', lazy='dynamic'), remote_side=[id], lazy='joined', foreign_keys=[reporting_manager_id])
+	
+	manager = db.relationship('Employees', backref=db.backref('subordinates', lazy='dynamic'), 
+		remote_side=[id], lazy='joined', foreign_keys=[reporting_manager_id])
 
-	def __init__(self, user_id, first_name, last_name, reporting_manager_id, designation, department,
-		general_leaves_remaining, medical_leaves_remaining, general_leaves_availed, medical_leaves_availed, 
-		date_of_joining):
+	def __init__(self, user_id, first_name, last_name, reporting_manager_id, designation, department, 
+		general_leaves_availed, medical_leaves_availed, date_of_joining):
 		self.user_id = user_id
 		self.first_name = first_name
 		self.last_name = last_name
 		self.reporting_manager_id = reporting_manager_id
 		self.designation = designation
 		self.department = department
-		self.general_leaves_remaining = general_leaves_remaining
-		self.medical_leaves_remaining = medical_leaves_remaining
 		self.general_leaves_availed = general_leaves_availed
 		self.medical_leaves_availed = medical_leaves_availed
 		self.date_of_joining = date_of_joining
@@ -72,20 +73,20 @@ class Balance_sheet(db.Model):
 	to_date = db.Column(db.Date(), nullable = False)
 	leave_type = db.Column(db.Enum('General','Medical'), nullable = False)
 	purpose = db.Column(db.String(200), nullable = False)
-	pay = db.Column(db.Enum('Payed','Unpayed'), nullable = False)
 	hr_remark = db.Column(db.String(128), nullable = True)
+	hr_approval = db.Column(db.Enum('Approved','Unapproved'), nullable = True)
 	manager_remark = db.Column(db.String(128), nullable = True)
 	manager_approval = db.Column(db.Enum('Approved','Unapproved'), nullable = True)
 
-	def __init__(self, emp_id, from_date, to_date, leave_type, purpose, pay, hr_remark, manager_remark,
+	def __init__(self, emp_id, from_date, to_date, leave_type, purpose, hr_remark, hr_approval, manager_remark,
 		manager_approval):
 		self.emp_id = emp_id
 		self.from_date = from_date
 		self.to_date = to_date
 		self.leave_type = leave_type
 		self.purpose = purpose
-		self.pay = pay
 		self.hr_remark = hr_remark
+		self.hr_approval = hr_approval
 		self.manager_remark = manager_remark
 		self.manager_approval = manager_approval
 
