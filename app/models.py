c6d2ea9f951e37dcfdb08bbc95095ce72ae22d7a
Salik Khan <m.salikkhan95@gmail.com>
2018-03-05 13:08:49 +0500

    Constructors removed

--- app/models.py
+++ app/models.py
@@ -53,18 +53,6 @@ class Employees(db.Model):
 	manager = db.relationship('Employees', backref=db.backref('subordinates', lazy='dynamic'), 
 		remote_side=[id], lazy='joined', foreign_keys=[reporting_manager_id])
 
-	def __init__(self, user_id, first_name, last_name, reporting_manager_id, designation, department, 
-		general_leaves_availed, medical_leaves_availed, date_of_joining):
-		self.user_id = user_id
-		self.first_name = first_name
-		self.last_name = last_name
-		self.reporting_manager_id = reporting_manager_id
-		self.designation = designation
-		self.department = department
-		self.general_leaves_availed = general_leaves_availed
-		self.medical_leaves_availed = medical_leaves_availed
-		self.date_of_joining = date_of_joining
-
 class Balance_sheet(db.Model):
 
 	id = db.Column(db.Integer, primary_key = True)
@@ -78,17 +66,6 @@ class Balance_sheet(db.Model):
 	manager_remark = db.Column(db.String(128), nullable = True)
 	manager_approval = db.Column(db.Enum('Approved','Unapproved'), nullable = True)
 
-	def __init__(self, emp_id, from_date, to_date, leave_type, purpose, hr_remark, hr_approval, manager_remark,
-		manager_approval):
-		self.emp_id = emp_id
-		self.from_date = from_date
-		self.to_date = to_date
-		self.leave_type = leave_type
-		self.purpose = purpose
-		self.hr_remark = hr_remark
-		self.hr_approval = hr_approval
-		self.manager_remark = manager_remark
-		self.manager_approval = manager_approval
 
 class Settings(db.Model):
 	id = db.Column(db.Integer, primary_key = True)
