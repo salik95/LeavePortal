cb6e154f9478271556836cb2a3d61a98be42939f
Salik Khan <m.salikkhan95@gmail.com>
2018-03-05 15:04:09 +0500

    Employee POST completed

--- app/controllers/employee.py
+++ app/controllers/employee.py
@@ -2,6 +2,7 @@ from app.models import *
 from app import db , app
 from flask import Blueprint, request, render_template, flash, g, session, redirect, url_for
 from flask_login import login_required, current_user
+from app.controllers.settings import settings_to_dict
 
 @app.route('/employee', methods=['GET', 'POST'])
 def employee():
@@ -9,14 +10,30 @@ def employee():
 		return "in progress"
 
 	if request.method == 'POST':
+
 		data_employee = request.get_json(force=True)
 
-		new_employee = Employees()
+		new_user = User(data_employee['email'], "chicken123", data_employee['role'])
+		db.session.add(new_user)
+		db.session.flush()
+		db.session.refresh(new_user)
 
+		del data_employee['email']
+		del data_employee['role']
+
+		data_employee['general_leaves_availed'] = '0'
+		data_employee['medical_leaves_availed'] = '0'
+		
+		data_employee['general_leaves_remaining'] = int(settings_to_dict()['general_leaves_limit'])
+		data_employee['medical_leaves_remaining'] = int(settings_to_dict()['medical_leaves_limit'])
+		
+		data_employee['user_id'] = new_user.id
+
+		new_employee = Employees()
 		key = list(data_employee.keys())
 		for item in key:
 			setattr(new_employee, item, data_employee[item])
-			
+
 		db.session.add(new_employee)
 		db.session.commit()
 		db.session.flush()
