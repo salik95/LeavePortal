e3b757882d59be1fe536d5bef5ccf9c94da5b03d
Salik Khan <m.salikkhan95@gmail.com>
2018-03-07 13:47:59 +0500

    HR and GM can view all employees leave history

--- app/controllers/leaves.py
+++ app/controllers/leaves.py
@@ -3,6 +3,7 @@ from app import db , app
 from flask import request, jsonify
 from flask_login import login_required, current_user
 from app.controllers.utilfunc import *
+from sqlalchemy import asc
 
 @app.route('/leave_form', methods=['POST'])
 def leave_form():
@@ -25,3 +26,20 @@ def leave_form():
 		leave_dict[col_name] = getattr(new_leave, col_name)
 	
 	return jsonify(leave_dict)
+
+#Revisit this fucntionality
+#=======================================================
+@app.route('/leave_all/super', methods=['GET'])
+def leave_all_super():
+	if current_user.role == 'HR Manager' or current_user.role == 'General Manager':
+		leave = Balance_sheet.query.order_by(asc(Balance_sheet.from_date)).all()
+		key = Balance_sheet.__mapper__.columns.keys()
+		my_list = []
+		for leave_item in leave:
+			my_dict = {}
+			for item in key:
+				my_dict[item] = getattr(leave_item, item)
+			my_list.append(my_dict)
+		return jsonify(my_list)
+	else:
+		return error_response_handler("Unauthorized access", 401)
