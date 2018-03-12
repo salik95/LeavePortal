a62877997ca68918c09f19715d077d4152d96eb4
Salik Khan <m.salikkhan95@gmail.com>
2018-03-12 13:52:34 +0500

    pending and responded requests refactored and sent on request_all and dashboard

--- app/controllers/dashboard.py
+++ app/controllers/dashboard.py
@@ -2,7 +2,7 @@ from app.models import *
 from app import db , app
 from flask import Blueprint, request, render_template, flash, g, session, redirect, url_for
 from flask_login import login_required, current_user
-from sqlalchemy import and_, or_
+from sqlalchemy import and_, or_, asc
 
 @app.route('/dashboard/', methods=['GET'])
 def dashboard():
@@ -11,21 +11,30 @@ def dashboard():
 		employee = current_user.employee
 
 		if current_user.role == "HR Manager":
+			requests = {}
 
-			manager_requests = db.session.query(Employees, Balance_sheet).join(Balance_sheet).filter(and_(Employees.reporting_manager_id == employee.id, Balance_sheet.manager_approval == None)).all()
+			pending = db.session.query(Employees, Balance_sheet).join(Balance_sheet).filter(Balance_sheet.hr_approval == None).order_by(asc(Balance_sheet.from_date)).all()
+			requests['pending'] = pending
+			if len(pending) < 5:
+				responded = db.session.query(Employees, Balance_sheet).join(Balance_sheet).filter(Balance_sheet.hr_approval != None).order_by(asc(Balance_sheet.from_date)).limit(5-len(pending))
+				requests['responded'] = responded
 
-			hr_requests = db.session.query(Employees, Balance_sheet).join(Balance_sheet).filter(Balance_sheet.hr_approval == None).all()
-
-			store.update({'manager_requests' : manager_requests, 'hr_requests' : hr_requests})
+			store.update({'requests' : requests})
 
 		else:
-			requests = db.session.query(Employees, Balance_sheet).join(Balance_sheet).filter(Employees.reporting_manager_id == employee.id)
-			if requests.first() is None:
+			all_requests = db.session.query(Employees, Balance_sheet).join(Balance_sheet).filter(Employees.reporting_manager_id == employee.id).all()
+			if all_requests.first() is None:
 				manager = False
 			else:
+				requests = {}
 				manager = True
-				pending_requests = requests.filter(Balance_sheet.manager_approval == None)
-				store.update({'pending_requests' : pending_requests})
+				pending = all_requests.filter(Balance_sheet.manager_approval == None)
+				requests['pending'] = pending
+				if len(pending) < 5:
+					responded = all_requests.filter(Balance_sheet.manager_approval != None).limit(5-len(pending))
+					requests['responded'] = responded
+
+				store.update({'requests' : requests})	
 			store.update({'manager' : manager})
 
 		leaves_details = {'general_leaves_availed' : employee.general_leaves_availed, 
--- app/controllers/leaves.py
+++ app/controllers/leaves.py
@@ -50,7 +50,6 @@ def leave_all():
 			temp_dict[item] = getattr(leave_item, item)
 		leave_list.append(temp_dict)
 
-
 	return render_template("all_leaves.html", data = leave_list)
 
 @app.route('/all_requests', methods=['GET'])
@@ -58,8 +57,8 @@ def request_all():
 	key = Balance_sheet.__mapper__.columns.keys()
 	
 	if current_user.role == "HR Manager":
-		pending = Balance_sheet.query.filter(Balance_sheet.hr_approval == None).order_by(asc(Balance_sheet.from_date))
-		responded = Balance_sheet.query.filter(Balance_sheet.hr_approval != None).order_by(asc(Balance_sheet.from_date))
+		pending = db.session.query(Employees, Balance_sheet).join(Balance_sheet).filter(Balance_sheet.hr_approval == None).order_by(asc(Balance_sheet.from_date))
+		responded = db.session.query(Employees, Balance_sheet).join(Balance_sheet).filter(Balance_sheet.hr_approval != None).order_by(asc(Balance_sheet.from_date))
 		
 		requests = {'pending' : get_dict_of_sqlalchemy_object(pending, key),
 		'responded' : get_dict_of_sqlalchemy_object(responded, key)}
@@ -73,6 +72,7 @@ def request_all():
 		requests = {'pending' : get_dict_of_sqlalchemy_object(pending, key, 'Balance_sheet'),
 		'responded' : get_dict_of_sqlalchemy_object(responded, key, 'Balance_sheet')}
 
+	print(requests)
 	return render_template("all_requests.html", data = requests)
 
 @app.route('/respond_request', methods=['PUT'])
